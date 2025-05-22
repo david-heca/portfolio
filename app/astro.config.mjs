af6edb0dd64ad42e4aa54ad7bebe3e401e192739
david-heca <davidherreradelcastillo2000@gmail.com>
2025-05-21 21:42:26 -0600

    config: dominio personalizado a davidheca.dev

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://david-heca.github.io',
+  site: 'https://davidheca.dev',
   base: '/portfolio',
 });
