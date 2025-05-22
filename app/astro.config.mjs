af7306bc739d0607f42ff7350db8b891231725dd
david-heca <davidherreradelcastillo2000@gmail.com>
2025-05-21 22:58:58 -0600

    feat: agregando logo (3d) y modificando layout general

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://davidheca.dev',
-  base: '/portfolio',
+  site: 'https://davidheca.dev'
 });
