7e7e5236d46b67fb0f59abb6cf43ef9d0ea720eb
david-heca <davidherreradelcastillo2000@gmail.com>
2026-02-22 10:05:48 -0600

    refactor: remove react-hot-toast integration and related components; simplify CV download button functionality

--- src/components/ui/CvDownloadButton.tsx
+++ src/components/ui/CvDownloadButton.tsx
@@ -1,13 +1,10 @@
 import { PiDownloadSimple } from "react-icons/pi";
-import { showCvDownloadToast } from "../../utils/toast";
 
 type Props = {
   href: string;
   download: string;
   className: string;
   label: string;
-  locale: "es" | "en";
-  cvName: "es" | "en";
 };
 
 export default function CvDownloadButton({
@@ -15,20 +12,9 @@ export default function CvDownloadButton({
   download,
   className,
   label,
-  locale,
-  cvName,
 }: Props) {
-  const handleClick = () => {
-    showCvDownloadToast({ locale, cvName });
-  };
-
   return (
-    <a
-      href={href}
-      download={download}
-      className={className}
-      onClick={handleClick}
-    >
+    <a href={href} download={download} className={className}>
       <PiDownloadSimple className="w-5 h-5" />
       {label}
     </a>
